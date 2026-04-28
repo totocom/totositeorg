fbd2c23952632f384701de3cf783684b55005e54
dodo jung <cktkdgh2580@gmail.com>
2026-04-29 00:08:03 +0700

    Fix scam report UUID validation

--- app/components/scam-report-form.tsx
+++ app/components/scam-report-form.tsx
@@ -135,7 +135,7 @@ function parseNumber(value: string) {
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
     value.trim(),
   );
 }
